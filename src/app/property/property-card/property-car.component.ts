d40cb7929fa891d6ba5389e0a3199c40d1019107
Pasan1998 <capofamerica@gmail.com>
2024-06-11 23:06:20 +0530

    6-11 changes

---
+++ src/app/property/property-card/property-car.component.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector : 'app-property-card',
+    template : 'property-card.Component.html',
+    styles : [ 'h1{font-weight:normal}']
+})
+
+export class PropertyCardComponent {
+
+}
